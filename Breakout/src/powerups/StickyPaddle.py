3c4b7dc7f76181086f2de8999af94a4e2d02c0c6
Coabest <coalbert_1@hotmail.com>
2023-04-05 17:15:38 -0400

    Adding base game files and a file for each new powerup

---
+++ Breakout/src/powerups/StickyPaddle.py
@@ -0,0 +1,32 @@
+"""
+ISPPJ1 2023
+Study Case: Breakout
+
+Author: Alejandro Mujica
+alejandro.j.mujic4@gmail.com
+
+This file contains the specialization of PowerUp to add two more ball to the game.
+"""
+import random
+from typing import TypeVar
+
+from gale.factory import Factory
+
+import settings
+from src.Ball import Ball
+from src.powerups.PowerUp import PowerUp
+
+
+class TwoMoreBall(PowerUp):
+    """
+    Power-up to Stick one ball to the paddle
+    """
+
+    def __init__(self, x: int, y: int) -> None:
+        # Set StickyPaddle Powerup frame other than 8
+        super().__init__(x, y, 8)
+        self.ball_factory = Factory(Ball)
+
+    def take(self, play_state: TypeVar("PlayState")) -> None:
+        # Define StickyPaddle behaviour
+        self.in_play = False
